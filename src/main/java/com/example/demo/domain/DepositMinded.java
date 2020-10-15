d420d055b4a0f9723e461a1bdc8e0ead14cc7964
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-15 17:34:43 +0900

    Create Domain Class about Deposit

---
+++ src/main/java/com/example/demo/domain/DepositMinded.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class DepositMinded {
+
+
+    private final Long depositId;
+
+}
