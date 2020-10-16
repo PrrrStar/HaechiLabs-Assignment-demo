97aa480ee8e09facf9324158fbfcd28d0a1c928f
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-16 21:46:04 +0900

    update business logic

--- src/main/java/com/example/demo/service/NotificationServiceImpl.java
+++ src/main/java/com/example/demo/service/NotificationServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.domain.Transaction;
 import com.example.demo.repository.NotificationRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class NotificationServiceImpl implements NotificationService{
 
     private NotificationRepository notificationRepository;
@@ -14,9 +16,56 @@ public class NotificationServiceImpl implements NotificationService{
     NotificationServiceImpl(NotificationRepository notificationRepository){
         this.notificationRepository = notificationRepository;
     }
-
+    /**
+     * 입출금 타입과 트랜잭션 상태로 찾기
+     * @param transferType
+     * @param txStatus
+     * @return
+     */
     @Override
-    public List<Transaction> findByTransferType(String transferType) {
-        return notificationRepository.findByTransferType(transferType);
+    public List<Transaction> retrieveTransactionByTypeAndStatus(String transferType, String txStatus) {
+        if (transferType == "DEPOSIT"){
+            if(txStatus == "MINDED"){
+                log.info("입출금 타입 {}\n트랜잭션 상태 {}",transferType,txStatus);
+                return notificationRepository.findByTransferTypeAndTxStatus("DEPOSIT","MINDED");
+            }else if(txStatus == "REPLACED"){
+                return notificationRepository.findByTransferTypeAndTxStatus("DEPOSIT","REPLACED");
+            }else if(txStatus == "CONFIRMED") {
+                return notificationRepository.findByTransferTypeAndTxStatus("DEPOSIT","CONFIRMED");
+            }else {
+                return null;
+            }
+        }else if(transferType == "WITHDRAWAL"){
+            if(txStatus == "PENDING"){
+                return notificationRepository.findByTransferTypeAndTxStatus("WITHDRAWAL","PENDING");
+            }else if(txStatus == "CONFIRMED") {
+                return notificationRepository.findByTransferTypeAndTxStatus("WITHDRAWAL","CONFIRMED");
+            }else {
+                return null;
+            }
+        }else{
+            return null;
+        }
     }
+
+
+    /**
+     * 입출금 타입으로 찾기
+     * @param transferType
+     * @return
+     */
+    @Override
+    public List<Transaction> retrieveTransactionByType(String transferType) {
+        if (transferType == "DEPOSIT"){
+            return notificationRepository.findByTransferType("DEPOSIT");
+        }else if(transferType == "WITHDRAWAL"){
+            return notificationRepository.findByTransferType("WITHDRAWAL");
+        }else{
+            return null;
+        }
+
+    }
+
+
+
 }
