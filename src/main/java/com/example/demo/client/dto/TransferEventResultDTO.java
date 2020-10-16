50aba35c96463ec8357701f070748e54b305f433
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-17 02:32:46 +0900

    Update DTO Using @JsonProperty

--- src/main/java/com/example/demo/client/dto/TransferEventResultDTO.java
+++ src/main/java/com/example/demo/client/dto/TransferEventResultDTO.java
@@ -1,61 +1,73 @@
 package com.example.demo.client.dto;
 
-import com.example.demo.client.TransferEventDeserializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
+import java.util.List;
 
 
 /**
  * 트랜잭션이 코인입출금 내역 에서 받아온 데이터를 정의한 클래스
  */
-@RequiredArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
-@JsonDeserialize(using = TransferEventDeserializer.class)
+
+
+@Data
 public class TransferEventResultDTO {
 
-    private final int id;
-    private final String from;
-    private final String to;
-    private final String amount;
-    private final String blockchain;
-    private final String status;
-    private final String confirmation;
-    private final String walletId;
-    private final String orgId;
-    private final String masterWalletId;
-    private final String transactionId;
-    private final String coinSymbol;
-    private final String transferType;
-    private final String blockHash;
-    private final String transactionHash;
-    private final String createdAt;
-    private final String updatedAt;
-    private final String walletName;
-    private final String walletType;
+    @JsonProperty("results")
+    private List<TransferEventResultDTO.Results> results;
+    @JsonProperty("pagination")
+    private TransferEventResultDTO.Pagination pagination;
 
-    // Json/JPA 를 위한 빈 생성자
-    TransferEventResultDTO(){
-        id = 1;
-        from = null;
-        to = null;
-        amount = null;
-        blockchain = null;
-        status = null;
-        confirmation = null;
-        walletId = null;
-        orgId = null;
-        masterWalletId = null;
-        transactionId = null;
-        coinSymbol = null;
-        transferType = null;
-        blockHash = null;
-        transactionHash = null;
-        createdAt = null;
-        updatedAt = null;
-        walletName = null;
-        walletType = null;
+    @Data
+    public static class Results {
+        @JsonProperty("walletType")
+        private String wallettype;
+        @JsonProperty("walletName")
+        private String walletname;
+        @JsonProperty("updatedAt")
+        private String updatedat;
+        @JsonProperty("createdAt")
+        private String createdat;
+        @JsonProperty("transactionHash")
+        private String transactionhash;
+        @JsonProperty("blockHash")
+        private String blockhash;
+        @JsonProperty("transferType")
+        private String transfertype;
+        @JsonProperty("coinSymbol")
+        private String coinsymbol;
+        @JsonProperty("transactionId")
+        private String transactionid;
+        @JsonProperty("masterWalletId")
+        private String masterwalletid;
+        @JsonProperty("orgId")
+        private String orgid;
+        @JsonProperty("walletId")
+        private String walletid;
+        @JsonProperty("confirmation")
+        private String confirmation;
+        @JsonProperty("status")
+        private String status;
+        @JsonProperty("blockchain")
+        private String blockchain;
+        @JsonProperty("amount")
+        private String amount;
+        @JsonProperty("to")
+        private String to;
+        @JsonProperty("from")
+        private String from;
+        @JsonProperty("id")
+        private int id;
+    }
 
+    @Data
+    public static class Pagination {
+        @JsonProperty("totalCount")
+        private int totalcount;
+        @JsonProperty("previousUrl")
+        private String previousUrl;
+        @JsonProperty("nextUrl")
+        private String nexturl;
     }
 }
