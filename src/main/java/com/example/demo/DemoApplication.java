2dcfa1ee79df4829fe7b0463be417853243a039d
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-18 23:22:14 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
