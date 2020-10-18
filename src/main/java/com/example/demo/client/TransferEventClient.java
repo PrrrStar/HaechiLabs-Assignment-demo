22e8148ca0ecf672201525ef85c47625a38db737
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-19 03:32:51 +0900

    Update TransferEventClient.java

--- src/main/java/com/example/demo/client/TransferEventClient.java
+++ src/main/java/com/example/demo/client/TransferEventClient.java
@@ -2,6 +2,7 @@ package com.example.demo.client;
 
 import com.example.demo.client.dto.TransferEventResultDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.ResponseEntity;
 
 /**
  * 코인 입출금 내역과 연결하는 인터페이스
@@ -10,4 +11,5 @@ public interface TransferEventClient {
 
     TransferEventResultDTO retrieveTransferEventResultDTO() throws JsonProcessingException;
 
+    ResponseEntity<String> retrieveTransferResults() throws JsonProcessingException;
 }
