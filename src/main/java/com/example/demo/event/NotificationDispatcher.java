6906cfe993879d665685e519bbdd10803d463705
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-19 03:34:09 +0900

    Create NotificationDispatcher.java

---
+++ src/main/java/com/example/demo/event/NotificationDispatcher.java
@@ -0,0 +1,13 @@
+package com.example.demo.event;
+
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NotificationDispatcher {
+
+}
