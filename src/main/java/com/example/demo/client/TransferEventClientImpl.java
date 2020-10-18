9203c0a754904151a3af1ee83be58f3fc9c8ed50
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-19 03:32:42 +0900

    Update TransferEventClientImpl.java

--- src/main/java/com/example/demo/client/TransferEventClientImpl.java
+++ src/main/java/com/example/demo/client/TransferEventClientImpl.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
 
 /**
  * 코인 입출금 내역과 REST로 연결하기 위한
@@ -23,25 +21,32 @@ public class TransferEventClientImpl implements TransferEventClient{
 
     private final RestTemplate restTemplate;
     private final HttpEntity<String> createHttpHeaders;
+    private final String valueTransferEventsHost;
 
     @Autowired
     public TransferEventClientImpl(final RestTemplate restTemplate,
-                                   final HttpEntity<String> createHttpHeaders){
+                                   final HttpEntity<String> createHttpHeaders,
+                                   @Value("${valueTransferEventsHost}") String valueTransferEventsHost){
         this.restTemplate = restTemplate;
         this.createHttpHeaders = createHttpHeaders;
+        this.valueTransferEventsHost = valueTransferEventsHost;
     }
 
     public TransferEventResultDTO retrieveTransferEventResultDTO() throws JsonProcessingException {
         String url = "http://localhost:3000/api/v2/eth/value-transfer-events";
 
-        ResponseEntity<String> response = restTemplate.exchange(URI.create(url), HttpMethod.GET, createHttpHeaders, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(valueTransferEventsHost, HttpMethod.GET, createHttpHeaders, String.class);
         ObjectMapper objectMapper = new ObjectMapper();
 
         TransferEventResultDTO transferEventResultDTO = objectMapper.readValue(response.getBody(), TransferEventResultDTO.class);
 
         return transferEventResultDTO;
-
-
     }
 
+    public ResponseEntity<String> retrieveTransferResults() throws JsonProcessingException {
+
+        ResponseEntity<String> response = restTemplate.exchange(valueTransferEventsHost, HttpMethod.GET, createHttpHeaders, String.class);
+
+        return response;
+    }
 }
