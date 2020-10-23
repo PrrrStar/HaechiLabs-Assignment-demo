7f6844b8f9d86a455eb322cdb6665474c8686d50
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-10-24 04:45:25 +0900

    Update NotificationController.java

--- src/main/java/com/example/demo/controller/NotificationController.java
+++ src/main/java/com/example/demo/controller/NotificationController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.client.dto.TransferEventResultDTO;
 import com.example.demo.domain.DepositConfirmed;
-import com.example.demo.domain.DepositMined;
 import com.example.demo.domain.WithdrawConfirmed;
 import com.example.demo.domain.WithdrawPending;
+import com.example.demo.event.RequestEvent;
+import com.example.demo.event.ResponseDepositMinedEvent;
+import com.example.demo.event.ResponseWithdrawPendingEvent;
 import com.example.demo.service.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,39 +22,48 @@ import java.util.List;
 @RequestMapping("/notifications")
 public class NotificationController {
 
+    private final MonitoringService monitoringService;
     private final NotificationService notificationService;
 
-    public NotificationController(final NotificationService notificationService){
+    public NotificationController(final MonitoringService monitoringService,
+                                  final NotificationService notificationService){
+        this.monitoringService = monitoringService;
         this.notificationService = notificationService;
 
     }
 
+    /**
+     * 3초마다 전체정보 조회 후 모니터링 서비스를 실행한다.
+     * @return
+     * @throws JsonProcessingException
+     */
     @GetMapping("/")
-    @Scheduled(fixedDelay = 3000)
+    @Scheduled(fixedDelay = 1000)
     public List<TransferEventResultDTO.Results> getAllTx() throws JsonProcessingException {
-        return notificationService.retrieveAllTxInfo();
+        return monitoringService.retrieveAllTxInfo();
     }
+
 
 
     @PostMapping("/deposit_mined")
-    public List<DepositMined> getDepositMinedTx(DepositMined depositMined){
-        return notificationService.retrieveDepositMinedTx(depositMined);
+    public ResponseDepositMinedEvent postDepositMinedTx(@RequestBody RequestEvent requestEvent){
+        return notificationService.retrieveDepositMinedTx(requestEvent);
     }
     @PostMapping("/deposit_reorged")
-    public List<TransferEventResultDTO.Results> getDepositReorgedTx() throws JsonProcessingException {
-        return notificationService.retrieveAllTxInfo();
+    public void postDepositReorgedTx(@RequestBody RequestEvent requestEvent) {
+        notificationService.retrieveDepositReorgedTx(requestEvent);
     }
     @PostMapping("/deposit_confirm")
-    public List<DepositConfirmed> getDepositConfirmTx(DepositConfirmed depositConfirmed){
-        return notificationService.retrieveDepositConfirmedTx(depositConfirmed);
+    public void postDepositConfirmTx(@RequestBody RequestEvent requestEvent){
+        notificationService.retrieveDepositConfirmedTx(requestEvent);
     }
     @PostMapping("/withdraw_pending")
-    public List<WithdrawPending> getWithdrawPendingTx(WithdrawPending withdrawPending){
-        return notificationService.retrieveWithdrawPendingTx(withdrawPending);
+    public ResponseWithdrawPendingEvent postWithdrawPendingTx(@RequestBody RequestEvent requestEvent){
+        return notificationService.retrieveWithdrawPendingTx(requestEvent);
     }
     @PostMapping("/withdraw_confirmed")
-    public List<WithdrawConfirmed> getWithdrawConfirmedTx(WithdrawConfirmed withdrawConfirmed){
-        return notificationService.retrieveWithdrawConfirmedTx(withdrawConfirmed);
+    public void postWithdrawConfirmedTx(@RequestBody RequestEvent requestEvent){
+        notificationService.retrieveWithdrawConfirmedTx(requestEvent);
     }
 
 
